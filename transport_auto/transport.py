cf1623c77b3cd28453328dd45a28e22e49e76b19
Luxas200 <justinaslukosius1@email.com>
2025-02-20 07:58:15 +0200

    2ver

--- transport_auto/transport.py
+++ transport_auto/transport.py
@@ -1,15 +1,37 @@
+from datetime import date
+from math import ceil
+
+
 class Driver:
-    def __init__(self, vacation: str, license_cat: list, wage_per_km: int):
+    def __init__(self,
+                 vacation: tuple [date, date],
+                 license_cat: list[str],
+                 wage_per_km: float
+                 ):
         self.vacation = vacation
         self.license_cat = license_cat
         self.wage_per_km = wage_per_km
 
+    def check_driver_availability(self):
+        today = date.today()
+        if self.vacation[0]<=today<=self.vacation[1]:
+            return False
+        return True
 
 class Transport:
-    def __init__(self, total_km: int, plate_number: str, fuel_type: str, insurance_exp: int,
-                 tech_inspection_exp: int, yearly_repair_exp: int, tech_inspection_date: str,
-                 insurance_date: str, available_cat: str, fuel_per_100_km: int, fuel_price: int):
-
+    def __init__(self,
+                 total_km: int,
+                 plate_number: str,
+                 fuel_type: str,
+                 insurance_exp: int,
+                 tech_inspection_exp: int,
+                 yearly_repair_exp: int,
+                 tech_inspection_date: date(year=2025,month=11,day=10),
+                 insurance_date: date(year=2025,month=6,day=20),
+                 available_cat: str,
+                 fuel_per_100_km: int,
+                 fuel_price: float,
+                 ):
         self.total_km = total_km
         self.plate_number = plate_number
         self.fuel_type = fuel_type
@@ -21,91 +43,163 @@ class Transport:
         self.available_cat = available_cat
         self.fuel_per_100_km = fuel_per_100_km
         self.fuel_price = fuel_price
-        self.fuel = True
 
-
-    def check_dates(self, insurance_date, tech_inspection_date):
-        import datetime
-        today = datetime.date.today()
-        current_month = today.month
-        print(current_month)
-        if current_month == insurance_date or tech_inspection_date:
-            return insurance_date and tech_inspection_date
-        return False
-
-    def total_exp(self, fuel_price: int, total_km: int, insurance_exp: int,
-                  tech_inspection_exp: int, yearly_repair_exp: int, fuel_per_100_km: int):
-        exploitation_exp = insurance_exp + tech_inspection_exp + yearly_repair_exp
-        fuel_exp = fuel_price * (total_km * fuel_per_100_km/100)
-        total_exp = exploitation_exp + fuel_exp
-        return total_exp
-
-    def check_driver_availability(self, vacation: str):
-        import datetime
-        today = datetime.date.today()
-        if today == vacation:
+    def check_dates(self):
+        today = date.today()
+        if today >= self.insurance_date:
+            return False
+        elif today >= self.tech_inspection_date:
             return False
         return True
 
+    def total_exp(self):
+        exploitation_exp = self.insurance_exp + self.tech_inspection_exp + self.yearly_repair_exp
+        fuel_exp = self.fuel_price * (self.total_km * self.fuel_per_100_km/100)
+        total_exp = exploitation_exp + fuel_exp
+        return total_exp
+
+    def count_price_km(self):
+        km_price = self.total_exp()/self.total_km
+        return km_price
 
 class Car(Transport):
-    def __init__(self, total_km: int, plate_number: str, fuel_type: str, insurance_exp: int, tech_inspection_exp: int,
-                 yearly_repair_exp: int, tech_inspection_date: str, insurance_date: str, available_cat: str,
-                 fuel_per_100_km: int, fuel_price):
+    def __init__(self,
+                 total_km: int,
+                 plate_number: str,
+                 fuel_type: str,
+                 insurance_exp: int,
+                 tech_inspection_exp: int,
+                 yearly_repair_exp: int,
+                 tech_inspection_date: date(year=2025, month=11, day=10),
+                 insurance_date: date(year=2025, month=6, day=20),
+                 available_cat: str,
+                 fuel_per_100_km: int,
+                 fuel_price: float,
+                 ):
         super().__init__(total_km, plate_number, fuel_type, insurance_exp, tech_inspection_exp, yearly_repair_exp,
                          tech_inspection_date, insurance_date, available_cat, fuel_per_100_km, fuel_price)
 
-
 class Bus(Transport):
-    def __init__(self, total_km: int, plate_number: str, fuel_type: str, insurance_exp: int, tech_inspection_exp: int,
-                 yearly_repair_exp: int, tech_inspection_date: str, insurance_date: str, available_cat: str,
-                 fuel_per_100_km: int, seats: int, persons: int, trip_length: int, fuel_price):
+    def __init__(self,
+                 total_km: int,
+                 plate_number: str,
+                 fuel_type: str,
+                 insurance_exp: int,
+                 tech_inspection_exp: int,
+                 yearly_repair_exp: int,
+                 tech_inspection_date: date(year=2025, month=11, day=10),
+                 insurance_date: date(year=2025, month=6, day=20),
+                 available_cat: str,
+                 fuel_per_100_km: int,
+                 seats: int,
+                 fuel_price: float,
+                 ):
         super().__init__(total_km, plate_number, fuel_type, insurance_exp, tech_inspection_exp, yearly_repair_exp,
                          tech_inspection_date, insurance_date, available_cat, fuel_per_100_km, fuel_price)
         self.seats = seats
-        self.persons = persons
-        self.trip_length = trip_length
-
-
 
     def count_bus_quantity(self, seats: int, persons: int):
-        result = round((seats / persons),0)
+        result = ceil(persons/seats)
         return result
 
-    def count_bus_estimate_price(self):
-        bus_quantity = self.count_bus_quantity(self.seats, self.persons)
-        price = bus_quantity * self.total_exp(self.fuel_price, self.trip_length, self.insurance_exp,
-                                            self.tech_inspection_exp, self.yearly_repair_exp, self.fuel_per_100_km)
+    def count_bus_trip_price(self, persons: int, trip_length: int, driver: Driver):
+        bus_quantity = ceil(persons/self.seats)
+        one_bus_trip_price = trip_length*self.count_price_km()+trip_length*driver.wage_per_km
+        price = bus_quantity * one_bus_trip_price
         return price
 
-
 class Truck(Transport):
-    def __init__(self, total_km: int, plate_number: str, fuel_type: str, insurance_exp: int, tech_inspection_exp: int,
-                 yearly_repair_exp: int, tech_inspection_date: str, insurance_date: str, available_cat: str,
-                 fuel_per_100_km: int, truck_load: int, trailer_load: int, is_hook: bool, fuel_price):
+    def __init__(self,
+                 total_km: int,
+                 plate_number: str,
+                 fuel_type: str,
+                 insurance_exp: int,
+                 tech_inspection_exp: int,
+                 yearly_repair_exp: int,
+                 tech_inspection_date: date(year=2025, month=11, day=10),
+                 insurance_date: date(year=2025, month=6, day=20),
+                 available_cat: str,
+                 fuel_per_100_km: int,
+                 truck_load: int,
+                 trailer_load: int,
+                 is_hook: bool,
+                 fuel_price: float,
+                 legal_trailer_cat: str
+                 ):
         super().__init__(total_km, plate_number, fuel_type, insurance_exp, tech_inspection_exp, yearly_repair_exp,
-                         tech_inspection_date, insurance_date, available_cat, fuel_per_100_km, fuel_price)
+                         tech_inspection_date, insurance_date, available_cat, fuel_per_100_km,fuel_price)
+
         self.truck_load = truck_load
         self.trailer_load = trailer_load
         self.is_hook = is_hook
+        self.legal_trailer_cat = legal_trailer_cat
 
-    def check_driver_trailers_license(self, license_cat: list):
-        legal_trailer_cat = 'CE'
-        for category in license_cat:
-            if legal_trailer_cat == category:
-                return True
-            return False
+    def check_driver_trailers_license(self, driver: Driver):
+        return self.legal_trailer_cat in driver.license_cat
 
+    def calculate_efficient_delivery(self, weight: int, ):
+        trips_with_trailer = ceil(weight/(self.truck_load+self.trailer_load))
+        trips_without_trailer = ceil(weight/self.truck_load)
+        if trips_with_trailer == trips_without_trailer:
+            print(f'Planned trips with trailer {trips_with_trailer} is the same as '
+                  f'trips without trailer {trips_without_trailer}. You have to drive without trailer')
+        else:
+            print(f'More efficient is to drive with trailer. '
+                  f'With trailer: {trips_with_trailer} trips < without trailer {trips_without_trailer} trips')
 
-driver = Driver('2025 06 30', ['A', 'B', 'CE', 'D'], 2)
-car = Car(1000, 'ABC123', 'Petrol', 200, 200,
-          1000, '2025 07 30', '2025 02 18', 'B', 8,
-          1)
-bus = Bus(1000, 'ABC123', 'Petrol', 200, 200,
-          1000, '2025 07 30', '2025 02 18', 'B', 8,
-          50, 30, 1000, 1)
-truck = Truck(1000, 'ABC123', 'Petrol', 200, 200,
-          1000, '2025 07 30', '2025 02 18', 'B', 8,
-              12, 12, True, 1)
-
+driver = Driver((date(year=2025, month=7, day=15), date(year=2025, month=7, day=30)),
+                ['A', 'B', 'CE', 'D'],
+                2
+                )
+car = Car(20000,
+          'ABC123',
+          'Petrol',
+          200,
+          20,
+          1000,
+          date(year=2025,month=2,day=20),
+          date(year=2025,month=2,day=18),
+          'B',
+          8,
+          1.49
+          )
+bus = Bus(80000,
+          'DDA123',
+          'Diesel',
+          1000,
+          200,
+          5000,
+          date(year=2025,month=10,day=15),
+          date(year=2025,month=2,day=18),
+          'D',
+          20,
+          50,
+          1.59,
+          )
+truck = Truck(120000,
+              'ZDA123',
+              'Diesel',
+              5000,
+              200,
+              10000,
+              date(year=2026, month=9, day=8),
+              date(year=2025, month=12, day=3),
+              'C',
+              30,
+              12,
+              5,
+              True,
+              1.59,
+              'CE'
+              )
+#selfcheck for functions:
+print(car.total_exp())
+print(car.check_dates())
+print(car.tech_inspection_date)
+print(driver.check_driver_availability())
+print(bus.count_bus_quantity(50,160))
+print(bus.count_price_km())
+print(bus.count_bus_trip_price(160,500,driver))
+print(truck.check_driver_trailers_license(driver))
+print(truck.calculate_efficient_delivery(37))
 
